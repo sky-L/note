fb4fce81050b54892a494fc4d1bb5d444eed4ebd
skyLee <690035384@qq.com>
2015-05-20 11:36:14 +0800

    Update job.sh

--- shell/job.sh
+++ shell/job.sh
@@ -1,7 +1,9 @@
+```
 #!/usr/bin/env bash
  CRON_NAME="cron"
  CRON_EXEC="/webserver/php/bin/php"
  CRON_FILE="/opt/application/vmall_task/action/MessagePushAction.php"
  #CRON_ARGV=""
  SLEEP_TIME=3
- . common/cron.sh
+ .cron.sh
+```
